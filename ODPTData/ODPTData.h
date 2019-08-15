7a7a0fbc6d5f279940509641c614573beda41450
Takehito Ikema <take@iii.xii.jp>
2019-08-15 17:00:04 +0900

    Initial Commit

---
+++ ODPTData/ODPTData.h
@@ -0,0 +1,19 @@
+//
+//  ODPTData.h
+//  ODPTData
+//
+//  Created by 池間健仁 on 2019/08/15.
+//  Copyright © 2019 Takehito Ikema. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for ODPTData.
+FOUNDATION_EXPORT double ODPTDataVersionNumber;
+
+//! Project version string for ODPTData.
+FOUNDATION_EXPORT const unsigned char ODPTDataVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ODPTData/PublicHeader.h>
+
+
---
+++ ODPTDataTests/ODPTDataTests.m
@@ -0,0 +1,37 @@
+//
+//  ODPTDataTests.m
+//  ODPTDataTests
+//
+//  Created by 池間健仁 on 2019/08/15.
+//  Copyright © 2019 Takehito Ikema. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface ODPTDataTests : XCTestCase
+
+@end
+
+@implementation ODPTDataTests
+
+- (void)setUp {
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
